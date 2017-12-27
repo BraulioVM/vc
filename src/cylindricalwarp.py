e6345be7f6090945c9e8bed1e4d0fa8976aa69a0
brlvldvlsmrtnz@gmail.com <brlvldvlsmrtnz@gmail.com>
2017-12-27 17:51:44 +0100

    "Arregla" implementacion de la deformacion cilindrica

--- src/cylindricalwarp.py
+++ src/cylindricalwarp.py
@@ -1,35 +1,50 @@
 import cv2
 import numpy as np
 
-from math import floor, tan
+from math import floor, tan, sqrt
 from util import plot_images, Image, show
 
 def cylindrical_warp(img, f=20):
     """Performs a cylindrical warp on a given image"""
+
+    # la implementacion viene de
+    # https://stackoverflow.com/questions/12017790/warp-image-to-appear-in-cylindrical-projection
+
+    def convert_coordinates(new_point, new_shape, f, r):
+        height, width = new_shape
+        
+        y, x = centered_point = new_point[0] - height//2, new_point[1] - width//2
+        
+        omega = width / 2
+        z0 = f - (r**2 - omega**2)**(1/2)
+
+        zc = (2*z0 + sqrt(4 * z0**2 - 4 * (x**2 / f**2 + 1) * (z0**2 - r**2)))/(2 * (x**2/f**2 + 1))
+
+
+        final_point = floor(y*zc/f + height//2), floor(x*zc/f + width//2)
+
+        return final_point
 
     height, width = img.shape
     new_img = np.zeros(img.shape, dtype=np.uint8)
 
     for row_index in range(len(img)):
         for col_index in range(len(img[0])):
-            c_row_index = floor(row_index - height*0.5) + 1
-            c_col_index = floor(col_index - width*0.5) + 1
+            y, x = convert_coordinates(
+                (row_index, col_index),
+                img.shape,
+                f,
+                f
+            )
 
-            img_col = f * tan(c_col_index / f)
-            
-            img_row = img_col * (1 + tan(c_row_index/f)**2) ** 0.5
-
-            img_col = floor(img_col + width*0.5) - 1
-            img_row = floor(img_row + height*0.5) - 1
-
-            if 0 <= img_col < width and 0 < img_row < height:
-                new_img[img_row][img_col] = img[row_index][col_index]
+            if 0 <= x < width and 0 < y < height:
+                new_img[row_index, col_index] = img[y, x]
 
     return new_img
                 
 
 def test_warp():
     """Computes and displays a cylindrical warp over a white image"""
-    img = np.ones((300, 300))
+    img = np.ones((600, 600))
 
     show(cylindrical_warp(img, f=600))
